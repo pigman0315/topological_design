1d248a7aaf530150ce2a4e9a53d5be05fc033cc1
Wen-Sheng <pigman031501@gmail.com>
2021-02-07 23:10:45 +0800

    step 4,5 ok

--- main.cpp
+++ main.cpp
@@ -105,7 +105,7 @@ int main(){
 		//
 		SolutionNode sn = sa.get_solution();
 		GVNS gvns(sn,cur_customers[i],cur_exch_point);
-		// gvns.run();
+		gvns.run();
 		cout << "--- step 2 --- " << endl;
 		gvns.solution.show();
 		solution_vec.push_back(gvns.solution);
@@ -126,7 +126,7 @@ int main(){
 		if(sn.routes_table.size() < owned_courier_num){
 			cout << "--- step 3 --- " << endl;
 			GVNS gvns(sn,cur_customers[i],cur_exch_point,owned_courier_num);
-			// gvns.run();
+			gvns.run();
 			gvns.solution.show();
 			solution_vec[i] = gvns.solution;
 		}
